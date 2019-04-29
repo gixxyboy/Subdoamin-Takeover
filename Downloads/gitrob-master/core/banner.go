fabaef3fa34f3510cd1d0d1972d5b304079de7c6
mohanx0x0 <mohan0x0@gmail.com>
2019-04-29 16:48:39 +0530

    Releasing v

---
+++ Downloads/gitrob-master/core/banner.go
@@ -0,0 +1,13 @@
+package core
+
+const (
+  Name        = "gitrob"
+  Version     = ""
+  Author      = "Michael Henriksen"
+  Website     = "https://github.com/michenriksen/gitrob"
+  ASCIIBanner = "        _ __           __\n" +
+    "  ___ _(_) /________  / /\n" +
+    " / _ `/ / __/ __/ _ \\/ _ \\\n" +
+    " \\_, /_/\\__/_/  \\___/_.__/\n" +
+    "/___/ by @michenriksen"
+)
